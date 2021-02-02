4d89ebf660bd5b9d0f6baae67581f12295b17504
David Polcari <dpolcari18@gmail.com>
2021-02-01 21:07:43 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,19 @@
-class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+require 'pry'
+
+class StudentsController < ApplicationController 
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
-  private
-
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  def activate
+    # binding.pry
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_back fallback_location: @student
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "david", last_name: "polcari")
